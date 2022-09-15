c09474dfdcf100393c13afb341f960f04b1ab6fd
3lLobo <flocwolf@gmail.com>
2022-09-15 20:46:01 +0200

    stucq in sol 🌞

--- backend/contracts/TotpAuthenticator.sol
+++ backend/contracts/TotpAuthenticator.sol
@@ -5,9 +5,6 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-struct Totp5 {
-    uint32 firstFive;
-}
 
 struct AuthData {
     // fist five digits of the 6-digit totp code tail-padded with a zero and finally parsed to a single number
@@ -31,36 +28,72 @@ struct Authentication {
 }
 
 contract TotpAuthenticator is Ownable {
-    mapping(uint256 => mapping(address => address)) requests;
+    // Counter provides requestId and increments with each request
+    uint256 requestCounter = 0;
+    // Maps a requestId to the requestor/validator and to the auth requested address
+    mapping(uint256 => address[2]) requests;
+    // Maps a requestId to a address and its response.
     mapping(uint256 => mapping(address => AuthResponse)) responses;
-    uint256[] requestIds;
+    
+
+    // Maps requestId to completes authentication
+    mapping(uint256 => Authentication) completedAuth;
 
     // TODO: define Events
 
     function setRequest(address _authAddress) public {
         // TODO: emit Event
+        requests[requestCounter] = [msg.sender, _authAddress];
+        requestCounter++;
+
     }
 
     function setResponse(uint256 _requestId, AuthData calldata _authData)
         public
     {
+        // TODO: get list of keys from response
+        // loop over keys and check that the sender has not submitted a response to this request yet
+        // or just override it:
+        responses[_requestId][msg.sender] = _authData;
+        
         // TODO: emit Event
+
     }
 
-    function getResponse() public view returns (AuthData memory) {
+    function getResponses(uint256 _requestId) public view returns (AuthData memory) {
         // Assert that caller created the AuthRequest
+        require(isValidator(_requestId), "This address did not submit the request.");
+        // Don't think it's allowed to return a mapping
+        return responses[_requestId];
     }
 
-    function authenticate() public {
+    // @param _requestId the id of the request
+    // @_responseAddress the address which submitted the valid response
+    function authenticate(uint32 _lastDigit, address _responseAddress) public {
         // Assert that caller created the AuthRequest
+        require(isValidator(_requestId), "This address did not submit the request.");
+        // TODO: 
+        // multiply totp5 by 10 and add _lastDigit,
+        // Cumpute sha256 hash and check agains stored hash
         // TODO: emit Event
+
     }
 
-    function getAuthentication() public view returns (bool) {}
+    function getAuthentication(uint256 _requestId) public view returns (Authentication) {
+        return completedAuth[_requestId];
+    }
 
     function resetAuthenticator() public onlyOwner {
         // TODO: emit Event
+        // TODO: delete everything
     }
 
-    function checkHash() private returns (bool) {}
+    function isValidator(uint256 _requestId) private returns (bool) {
+        return msg.sender = requests[_requestId][0];
+    }
+
+    function checkHash(uint32 _totp5, uint32 _lastDigit, ) private returns (bool) {
+        // TODO: the hashing check
+        // What type is sha256
+    }
 }
