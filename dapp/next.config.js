37c3148fb8305acbcd0acb01e654c4117f2787be
daniecon <daniel.contreras@math.ethz.ch>
2022-09-22 13:31:05 +0200

    Config env variables

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config()
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,6 +8,9 @@ const nextConfig = {
 
     return config
   },
+  env: {
+    API_KEY: process.env.API_KEY
+  }
 }
 
 module.exports = nextConfig
