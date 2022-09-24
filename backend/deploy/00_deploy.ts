4060d8803942aa41ae0ee3ca083deda017ddb413
daniecon <daniel.contreras@math.ethz.ch>
2022-09-24 17:21:00 +0200

    Deploy modified contarcts

--- backend/deploy/00_deploy.ts
+++ backend/deploy/00_deploy.ts
@@ -9,15 +9,32 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const { owner } = await getNamedAccounts()
 
+  const HashCheckVerifier = await deploy('HashCheckVerifier', {
+    from: owner,
+    log: true,
+    autoMine: true,
+  })
+
+  const OtpMerkleTreeVerifier = await deploy('OtpMerkleTreeVerifier', {
+    from: owner,
+    log: true,
+    autoMine: true,
+  })
+
   const ZkWalletFactory = await deploy('ZkWalletFactory', {
     from: owner,
+    args: [HashCheckVerifier.address],
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
   })
 
   fs.writeFileSync(
     './config.ts',
-    `export const zkWalletFactoryAddress = "${ZkWalletFactory.address}"\n`
+    `export const address = { 
+      zkWalletFactory: "${ZkWalletFactory.address}",
+      HashCheckVerifier: "${HashCheckVerifier.address}",
+      OtpMerkleTreeVerifier: "${OtpMerkleTreeVerifier.address}",
+    }\n`
   )
 }
 export default func
