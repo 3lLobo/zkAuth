53880e7c9522ad135ebb5caebeb8e0aa552b000b
Rishabh Gupta <rishotics@gmail.com>
2022-09-27 01:22:30 +0530

    add comments

--- backend/contracts/ZkWalletFactory.sol
+++ backend/contracts/ZkWalletFactory.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.17;
 
 import './ZkSocialRecoveryWallet.sol';
 
+/// @title ZkWalletFactory
+/// @author ZK Authentication team
+/// @notice You can use this contract for deploying wallets having otp verification and social recovery
+/// @dev All function calls are currently implemented without side effects
 contract ZkWalletFactory {
   mapping(address => address) public userAddressToWalletAddress;
   event WalletCreated(address walletAddress);
@@ -13,6 +17,14 @@ contract ZkWalletFactory {
     hashCheckVerifier = _hashCheckVerifier;
   }
 
+  /**
+   * @notice Deploy a new wallet
+   * @param _ownerPasswordHash password is not stored directly. We store the poseidon(public key user, password)
+   * @param _thresholdForRecovery Minimum number of votes a new owner needs to get for execution
+   * @param _otpVerifier address of the otp verifier contract
+   * @param root merkle root value unique to a wallet which is later used in otp verification
+   * @return walletAddress address of new wallet
+   */
   function deployWallet(
     uint256 _ownerPasswordHash,
     uint256 _thresholdForRecovery,
@@ -32,7 +44,16 @@ contract ZkWalletFactory {
     emit WalletCreated(walletAddress);
   }
 
-  function getUserWalletAddress(address _user) external view returns(address _walletAddress) {
+  /**
+   * @notice Getter function to get user wallet address
+   * @param _user address
+   * @return walletAddress address of user wallet
+   */
+  function getUserWalletAddress(address _user)
+    external
+    view
+    returns (address _walletAddress)
+  {
     _walletAddress = userAddressToWalletAddress[_user];
   }
 }
