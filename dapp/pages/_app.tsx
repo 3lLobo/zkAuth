d93868f327505c1e9f7e77a48310552b007e5a2c
3lLobo <flocwolf@gmail.com>
2022-09-26 21:36:47 +0200

    Ready for Live deploy✅

--- dapp/pages/_app.tsx
+++ dapp/pages/_app.tsx
@@ -2,18 +2,35 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '../components'
 import { ThemeProvider } from 'next-themes'
-import { DAppProvider, Localhost } from '@usedapp/core'
+import { DAppProvider, Localhost, Chain } from '@usedapp/core'
 
 import { Provider as CeramicProvider } from '@self.id/react'
 
-const optGoerliChainId = 420
-const optGoerliUrl = 'https://goerli.optimism.io'
-const config = {
-  readOnlyChainId: optGoerliChainId,
-  readOnlyUrls: {
-    [optGoerliChainId]: optGoerliUrl,
-    [Localhost.chainId]: 'http://127.0.0.1:8545',
+export const OptimismGoerli: Chain = {
+  chainId: 420,
+  chainName: 'Optimism Goerli Testnet',
+  isTestChain: true,
+  isLocalChain: false,
+  multicallAddress: '0x0000000000000000000000000000000000000000',
+  getExplorerAddressLink: (address: string) =>
+    `https://blockscout.com/optimism/goerli//address/${address}`,
+  getExplorerTransactionLink: (transactionHash: string) =>
+    `https://blockscout.com/optimism/goerli//tx/${transactionHash}`,
+  // Optional parameters:
+  rpcUrl: 'https://goerli.optimism.io',
+  blockExplorerUrl: 'https://blockscout.com/optimism/goerli',
+  nativeCurrency: {
+    name: 'Optimism Goerli Testnet',
+    symbol: 'ETH',
+    decimals: 18,
   },
+}
+const config: any = {
+  readOnlyChainId: OptimismGoerli.chainId,
+  readOnlyUrls: {
+    [OptimismGoerli.chainId]: OptimismGoerli.rpcUrl,
+  },
+  networks: [OptimismGoerli],
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
