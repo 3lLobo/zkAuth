29bc951f604d9aba8cc7b7a5c28af2f1347ee862
3lLobo <flocwolf@gmail.com>
2022-10-03 16:51:36 +0200

    new encryption + more hashes 🐳

--- dapp/pages/demoEncryption.jsx
+++ dapp/pages/demoEncryption.jsx
@@ -5,7 +5,6 @@ import { useEffect } from 'react'
 import { decryptOrSignMetamask, encryptMetamask } from 'helpers/utils'
 import { useState, SetStateAction } from 'react'
 import { useCeramic } from '../hooks/useCeramic'
-import { recoverAddress } from 'ethers/lib/utils'
 
 function demoEncryption() {
   const [ceramicAddress, setCeramicAddress] = useState()
@@ -13,13 +12,14 @@ function demoEncryption() {
   const { ceramicData, ceramicStatus } = useCeramic(ceramicAddress)
 
   useEffect(() => {
+    const address = '0x369551E7c1D29756e18BA4Ed7f85f2E6663e1e8d'
     if (!ceramicAddress) {
-      setCeramicAddress('0x369551E7c1D29756e18BA4Ed7f85f2E6663e1e8d')
+      setCeramicAddress(address)
     }
     console.log('Record: ', ceramicData)
     console.log('ceramicStatus', ceramicStatus)
 
-    // @danial How to read the data
+    // @daniel How to read the data
     if (ceramicData.content) {
       console.log('Read the hashes: ', ceramicData.content.MerkleTree)
     }
@@ -33,16 +33,14 @@ function demoEncryption() {
     activateBrowserWallet()
   }, [activateBrowserWallet])
 
-  // prepareMerkleTree()
-
   return (
     <>
       <div className="flex justify-center w-full max-w-7xl lg:px-8">
         <div className="w-full flex flex-col justify-center place-items-center bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">
           <button
             onClick={() => {
-              signMsg(topSecret, setText, setButtonText, ceramicData)
-              // prepareMerkleTree()
+              // signMsg(topSecret, setText, setButtonText, ceramicData)
+              prepareMerkleTree('0x369551E7c1D29756e18BA4Ed7f85f2E6663e1e8d')
             }}
             className=" w-fit px-6 py-1 bg-violet-400 dark:bg-violet-500 hover:bg-violet-500 rounded-3xl m-3"
           >
